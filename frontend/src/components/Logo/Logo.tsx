33ea248485cc0830ccd90db6d2be5a95bb74162b
JakubFolta <jakubfolta@yahoo.co.uk>
2023-06-22 12:47:40 +0100

    Add logo versions and add responsiveness to logo component

--- frontend/src/components/Logo/Logo.tsx
+++ frontend/src/components/Logo/Logo.tsx
@@ -1,8 +1,15 @@
 import { StyledLogo, StyledLogoContainer } from "./styles";
-import logo from "../../assets/images/logo-maluszkowo.png";
+import mobileLogo from "../../assets/images/logo-maluszkowo-mobile.png";
+import tabletLogo from "../../assets/images/logo-maluszkowo-tablet.png";
+import desktopLogo from "../../assets/images/logo-maluszkowo-desktop.png";
 
 export const Logo: React.FC = () => (
   <StyledLogoContainer>
-    <StyledLogo src={logo} />
+    <StyledLogo
+      srcSet={`${mobileLogo} 90w, ${tabletLogo} 120w, ${desktopLogo} 140w`}
+      sizes="(max-width: 640px) 90px, (max-width: 1024px) 120px, 140px"
+      src={desktopLogo}
+      alt="Maluszkowo logo"
+    />
   </StyledLogoContainer>
 )
