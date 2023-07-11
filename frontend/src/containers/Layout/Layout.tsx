5ededd60e17a4123fc845d30ec389dac0e925642
JakubFolta <jakubfolta@yahoo.co.uk>
2023-07-11 14:40:25 +0100

    Add position fixed to header

--- frontend/src/containers/Layout/Layout.tsx
+++ frontend/src/containers/Layout/Layout.tsx
@@ -1,16 +1,33 @@
+import { debounce } from "lodash-es";
 import { Header } from "../../components";
 import { Footer } from "../Footer/Footer";
+import { useEffect, useState } from "react";
+import { MainContainer } from "./styles";
 
 interface Props {
   children?: React.ReactNode;
 }
 
-export const Layout: React.FC<Props> = ({ children }) => (
-  <>
-    <Header />
-      <main>
+export const Layout: React.FC<Props> = ({ children }) => {
+  const [headerHeight, setHeaderHeight] = useState(-1);
+
+  useEffect(() => {
+    getHeaderHeight();
+    window.addEventListener('resize', getHeaderHeight);
+  }, [])
+
+  const getHeaderHeight = debounce(() => {
+    const headHeight = document.getElementById('header')!.clientHeight;
+    setHeaderHeight(headHeight);
+  }, 300);
+
+  return (
+    <>
+      <Header />
+      <MainContainer headerHeight={headerHeight}>
         {children}
-      </main>
-    <Footer />
-  </>
-);
+      </MainContainer>
+      <Footer />
+    </>
+  );
+}
