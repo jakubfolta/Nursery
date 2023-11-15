3e372ab2a8344aa19fa97d578059c24e95dcb111
JakubFolta <jakubfolta@yahoo.co.uk>
2023-11-15 11:40:54 +0000

    Add responsiveness to Layout

--- frontend/src/containers/pages/MainPage/styles.ts
+++ frontend/src/containers/pages/MainPage/styles.ts
@@ -2,7 +2,12 @@ import styled from "styled-components";
 
 export const StyledHero = styled.div`
   height: 60vh;
-  background-color: var(--color-accent);
+  /* background-color: var(--color-accent); */
+  max-width: 540px;
+  padding: 0 1rem;
+
+  @media only screen and (min-width: 768px) { max-width: 768px; }
+  @media only screen and (min-width: 1024px) { max-width: 1024px; }
 `
 
 export const StyledHeading = styled.h1`
