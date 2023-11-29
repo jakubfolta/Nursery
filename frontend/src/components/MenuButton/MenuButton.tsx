91745e42bf4107caf65fdb513be9e17d801f8a3a
JakubFolta <jakubfolta@yahoo.co.uk>
2023-11-29 08:56:16 +0000

    Fix visibility of menu scrolling up when route change and refactor/removing unnecessary code

--- frontend/src/components/MenuButton/MenuButton.tsx
+++ frontend/src/components/MenuButton/MenuButton.tsx
@@ -3,7 +3,7 @@ import { Props } from "./interfaces";
 
 export const MenuButton: React.FC<Props> = props => (
   <ButtonContainer onClick={props.onButtonClick}>
-    <Line1 className={['menu-button', props.buttonState].join(' ')} />
-    <Line2 className={['menu-button', props.buttonState].join(' ')} />
+    <Line1 className={props.buttonState} />
+    <Line2 className={props.buttonState} />
   </ButtonContainer>
 )
