e13d830313564e385e13c8f9fceae510d28f6863
JakubFolta <jakubfolta@yahoo.co.uk>
2024-03-11 08:52:27 +0100

    Add redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense, useCallback, useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Navigate } from 'react-router-dom';
 import { CONSTANTS, GlobalStyle } from './styles/global';
 import { Layout } from './components';
 import WebpageContextProvider from './store/webpage-context';
@@ -218,6 +218,7 @@ export const App: React.FC = () => {
         <Suspense fallback={<div>Loading...</div>}>
           <Routes location={displayLocation}>
             {routes}
+            <Route path='/*' element={<Navigate to="/" />} />
           </Routes>
         </Suspense>
       </Layout>
